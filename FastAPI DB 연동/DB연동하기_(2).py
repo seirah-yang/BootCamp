c4abeab94f2b367a8683205896eae6055b6e4163
Sora Yang, Seirah <nftsgsrz3@gmail.com>
2025-09-02 11:02:00 +0900

    Update and rename DB연동하기_(2) to DB연동하기_(2).py

--- FastAPI DB 연동/DB연동하기_(2).py
+++ FastAPI DB 연동/DB연동하기_(2).py
@@ -1,4 +1,4 @@
-```bash
+
 from sqlalchemy import create_engine
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
@@ -24,4 +24,3 @@ def sql(request:Request):
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port = 8383)
-```
